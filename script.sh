e2805b96afb25744deb1b4e4e5a44e71fefa9f70
Stéphane Bressani <stephane@bressani.dev>
2024-04-16 14:46:25 +0200

    ok

--- script.sh
+++ script.sh
@@ -11,47 +11,38 @@
 #export OS_IDENTITY_API_VERSION="3"
 #swift list
 
-## Configuration
 CONTAINER_NAME="default"
 DIRECTORY_NAME="MAO_001"
 
-# Créer un fichier temporaire pour stocker la liste des fichiers
 swift list $CONTAINER_NAME --prefix $DIRECTORY_NAME/ > files_to_check.txt
 
 grep '\.sha256$' files_to_check.txt | while read sha256_file; do
     echo "Traitement de $sha256_file..."
 
-    # Préparer les chemins locaux pour le téléchargement
     sha256_file_path="temp_downloads/$sha256_file"
+    actual_file_path="temp_downloads/${sha256_file%.sha256}"
+    
+
+    if [ -d "$directory_path" ]; then
+        rm -rf "$directory_path"
+    fi
     mkdir -p "$(dirname "$sha256_file_path")"
 
-    # Télécharger le fichier .sha256
-#    swift download $CONTAINER_NAME --output "$sha256_file_path" "$sha256_file"
-# Télécharger le fichier avec pv pour montrer la progression
-#   swift download $CONTAINER_NAME "$sha256_file" --output - | pv > "$sha256_file_path"
-   swift download $CONTAINER_NAME "$sha256_file" --output "$sha256_file_path"
+    # swift download $CONTAINER_NAME --output "$sha256_file_path" "$sha256_file"
+    swift download $CONTAINER_NAME "$sha256_file" --output - | pv > "$sha256_file_path"
 
+    # swift download $CONTAINER_NAME --output "$actual_file_path" "${sha256_file%.sha256}"
+    swift download $CONTAINER_NAME  "${sha256_file%.sha256}" --output - | pv > "$actual_file_path"
 
-    # Extraire le nom du fichier réel et son chemin à partir du nom du fichier .sha256
-    actual_file_path="temp_downloads/${sha256_file%.sha256}"
-
-    # Télécharger le fichier correspondant
-    swift download $CONTAINER_NAME --output "$actual_file_path" "${sha256_file%.sha256}"
-
-    # Calculer le checksum SHA-256 du fichier téléchargé
-    calculated_checksum=$(sha256sum "$actual_file_path" | awk '{print $1}')
-
-    # Lire le checksum attendu à partir du fichier .sha256
-    expected_checksum=$(cat "$sha256_file_path")
-
-    # Comparer les checksums
-    if [[ "$calculated_checksum" == "$expected_checksum" ]]; then
-        echo "$actual_file_path: SUCCESS"
+    pushd "$(dirname "$actual_file_path")"
+    if shasum -a 256 -c "$(basename "$sha256_file_path")"; then
+        echo -e "\e[32m$(basename "$sha256_file_path"): SUCCESS\e[0m"
     else
-        echo "$actual_file_path: CHECKSUM MISMATCH"
+        echo -e "\e[31m$(basename "$sha256_file_path"): CHECKSUM MISMATCH\e[0m"
     fi
+    popd
 
-    # Supprimer les fichiers téléchargés pour économiser de l'espace disque
+
     rm "$sha256_file_path"
     rm "$actual_file_path"
 done
