6c8c81ed9bbe21c3eab48a04c2430c9f8548b0f0
Stéphane Bressani <stephane@bressani.dev>
2024-04-16 13:23:55 +0200

    ok

---
+++ script.sh
@@ -0,0 +1,11 @@
+#pip install python-swiftclient
+#pip install python-keystoneclient
+#export OS_AUTH_URL="https://swiss-backup02.infomaniak.com/identity/v3"
+#export OS_PROJECT_NAME="sb_project_SBI-****"
+#export OS_USER_DOMAIN_NAME="default"
+#export OS_PROJECT_DOMAIN_NAME="default"
+#export OS_USERNAME="SBI-****"
+#export OS_PASSWORD="*****"
+#export OS_REGION_NAME="RegionOne"
+#export OS_IDENTITY_API_VERSION="3"
+swift list
